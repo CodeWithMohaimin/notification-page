784c4a2fd81c595b49332f25164ce453c9bd8dd7
programmermohaimin <dev.mohaimin@gmail.com>
2022-11-03 08:08:20 +0600

    start media queries

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const btn = document.getElementById('btn');
+const notification = document.getElementById('toggle_notification');
+
+btn.addEventListener('click', function () {
+
+    notification.classList.toggle('active')
+        
+})
